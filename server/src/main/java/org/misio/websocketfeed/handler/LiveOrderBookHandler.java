c8448f43068dd726ca5a2d44f16e6ce57aaae88f
Marcin Misinski <marbimbo@gmail.com>
2021-02-11 00:29:24 +0100

    clean up

--- server/src/main/java/org/misio/websocketfeed/handler/LiveOrderBookHandler.java
+++
@@ -1,7 +0,0 @@
-package org.misio.websocketfeed.handler;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-public interface LiveOrderBookHandler {
-    void handleMessages(String message) throws JsonProcessingException;
-}
